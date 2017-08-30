37d30a4be1f6d6af580a0496da49636b7e6e94f0
Tomás Del Bianco <tomasdelbianco@gmail.com>
2017-08-29 21:49:13 -0300

    [IMP] Títulos

--- addons/theme_rosariomapas/static/js/maps.js
+++ addons/theme_rosariomapas/static/js/maps.js
@@ -309,6 +309,7 @@ function createHomepageGoogleMap(_latitude,_longitude,json, action){
                 map.fitBounds(bounds);
                 if (data && data.url){
                     window.history.pushState({},"", "/recorrido/"+data.url);
+                    document.title = "Recorrido de la línea de colectivo " + data.name + " en el mapa de Rosario"
                 }
             });
         }
